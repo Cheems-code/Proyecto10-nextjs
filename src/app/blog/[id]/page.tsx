5da51005f4932e2ac65ef8e46fed5ee66ddd64e7
Cheems-code <osmar.ancco@tecsup.edu.pe>
2025-06-22 21:50:14 -0500

    update blog

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,1 +1,14 @@
-//pagina no disponible
+export default function BlogPostStatic() {
+  return (
+    <section className="py-12">
+      <div className="max-w-3xl mx-auto">
+        <div className="bg-white shadow-md rounded-lg p-6 border border-gray-200">
+          <h3 className="text-2xl font-bold text-gray-800 mb-2">Post del Blog</h3>
+          <p className="text-gray-700 whitespace-pre-line">
+            Ejemplo Blog.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
