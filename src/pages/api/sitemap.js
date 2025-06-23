50af5e7e84007528208038213c875c991785c9cb
Cheems-code <osmar.ancco@tecsup.edu.pe>
2025-06-22 22:13:28 -0500

    fix sitemap

--- src/pages/api/sitemap.js
+++ src/pages/api/sitemap.js
@@ -1,23 +1,34 @@
+async function getAllBlogPosts() {
+  return [
+    { id: 1 },
+    { id: 2 },
+    { id: 3 }
+  ];
+}
+
 export default async function handler(req, res) {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://proyecto10-nextjs.onrender.com';
-  const posts = await getAllBlogPosts(); // Debe devolver [{ id: 1 }, ...]
-  const staticPages = ['', 'about', 'contact'];
+  try {
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://proyecto10-nextjs.onrender.com';
+    const posts = await getAllBlogPosts();
+    const staticPages = ['', 'about', 'contact'];
 
-  let urls = staticPages
-    .map((page) => `<url><loc>${baseUrl}/${page}</loc></url>`)
-    .join('');
+    let urls = staticPages
+      .map((page) => `<url><loc>${baseUrl}/${page}</loc></url>`)
+      .join('');
 
-  let blogUrls = posts
-    .map((post) => `<url><loc>${baseUrl}/blog/${post.id}</loc></url>`)
-    .join('');
+    let blogUrls = posts
+      .map((post) => `<url><loc>${baseUrl}/blog/${post.id}</loc></url>`)
+      .join('');
 
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${urls}
-    ${blogUrls}
-  </urlset>`;
+    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${urls}
+      ${blogUrls}
+    </urlset>`;
 
-  res.setHeader('Content-Type', 'application/xml');
-  res.write(sitemap);
-  res.end();
+    res.setHeader('Content-Type', 'application/xml');
+    res.status(200).send(sitemap);
+  } catch (error) {
+    res.status(500).send('Internal Server Error');
+  }
 }
